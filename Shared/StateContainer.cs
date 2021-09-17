7e0de5ab20b61a2978bf44709b6bbaa6c7e4b2cd
kimux8 <60120007+kimux8@users.noreply.github.com>
2021-09-17 22:33:30 +0900

    Initial commit

---
+++ Shared/StateContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDarkMode.Shared
+{
+    public class StateContainer
+    {
+        private string savedString;
+
+        public string Property
+        {
+            get => savedString;
+            set
+            {
+                savedString = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public event Action OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
